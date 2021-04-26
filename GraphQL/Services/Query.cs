0b00a07de2bd46ba2198d5912a9be4705454ee91
szakri <szabokristof2020@gmail.com>
2021-04-26 19:01:58 +0200

    Make functions async

--- GraphQL/Services/Query.cs
+++ GraphQL/Services/Query.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using HotChocolate.Data;
 using Common.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace GraphQL.Services
 {
@@ -17,47 +18,47 @@ namespace GraphQL.Services
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Student> GetStudents([ScopedService] SchoolContext context) =>
-            context.Students.Include(s => s.Courses);
+        public async Task<IEnumerable<Student>> GetStudents([ScopedService] SchoolContext context) =>
+            await context.Students.Include(s => s.Courses).ToListAsync();
 
         [UseDbContext(typeof(SchoolContext))]
-        public Student GetStudent([ScopedService] SchoolContext context, int id) =>
-            context.Students.Include(s => s.Courses).FirstOrDefault(s => s.StudentId == id);
-
-        [UseDbContext(typeof(SchoolContext))]
-        [UsePaging]
-        [UseProjection]
-        [UseFiltering]
-        [UseSorting]
-        public IQueryable<Course> GetCourses([ScopedService] SchoolContext context) =>
-            context.Courses.Include(c => c.Subject);
-
-        [UseDbContext(typeof(SchoolContext))]
-        public Course GetCourse([ScopedService] SchoolContext context, int id) =>
-            context.Courses.Include(c => c.Subject).FirstOrDefault(c => c.CourseId == id);
+        public async Task<Student> GetStudent([ScopedService] SchoolContext context, int id) =>
+            await context.Students.Include(s => s.Courses).FirstOrDefaultAsync(s => s.StudentId == id);
 
         [UseDbContext(typeof(SchoolContext))]
         [UsePaging]
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Subject> GetSubjects([ScopedService] SchoolContext context) =>
-            context.Subjects;
+        public async Task<IEnumerable<Course>> GetCourses([ScopedService] SchoolContext context) =>
+            await context.Courses.Include(c => c.Subject).Include(c => c.Students).ToListAsync();
 
         [UseDbContext(typeof(SchoolContext))]
-        public Subject GetSubject([ScopedService] SchoolContext context, int id) =>
-            context.Subjects.Find(id);
+        public async Task<Course> GetCourse([ScopedService] SchoolContext context, int id) =>
+            await context.Courses.Include(c => c.Subject).Include(c => c.Students).FirstOrDefaultAsync(c => c.CourseId == id);
 
         [UseDbContext(typeof(SchoolContext))]
         [UsePaging]
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Attendance> GetAttendances([ScopedService] SchoolContext context) =>
-            context.Attendances.Include(a => a.Course).Include(a => a.Student);
+        public async Task<IEnumerable<Subject>> GetSubjects([ScopedService] SchoolContext context) =>
+            await context.Subjects.Include(s => s.Courses).ToListAsync();
 
         [UseDbContext(typeof(SchoolContext))]
-        public Attendance GetAttendance([ScopedService] SchoolContext context, int id) =>
-            context.Attendances.Include(a => a.Course).Include(a => a.Student).FirstOrDefault(a => a.AttendanceId == id);
+        public async Task<Subject> GetSubject([ScopedService] SchoolContext context, int id) =>
+            await context.Subjects.Include(s => s.Courses).FirstOrDefaultAsync(s => s.SubjectId == id);
+
+        [UseDbContext(typeof(SchoolContext))]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public async Task<IEnumerable<Attendance>> GetAttendances([ScopedService] SchoolContext context) =>
+            await context.Attendances.Include(a => a.Course).Include(a => a.Student).ToListAsync();
+
+        [UseDbContext(typeof(SchoolContext))]
+        public async Task<Attendance> GetAttendance([ScopedService] SchoolContext context, int id) =>
+            await context.Attendances.Include(a => a.Course).Include(a => a.Student).FirstOrDefaultAsync(a => a.AttendanceId == id);
     }
 }
