ede087af75af84f49203cea1da895eb6203154cd
szakri <szabokristof2020@gmail.com>
2021-11-01 17:28:08 +0100

    Change startup url

--- OData/Startup.cs
+++ OData/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OData.Edm;
 using System.Linq;
 
